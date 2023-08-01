0a14d26414dc4b92449ad2a9c0b43ebb76c2f36d
ishanks <ishanks@adobe.com>
2023-08-01 19:16:53 +0530

    counter update

--- counter.js
+++ counter.js
@@ -1,25 +1,30 @@
 import  {take}  from "rxjs/operators/take";
 import sub,{genString} from './sub'
-const MAX_ITER = 10000
+const MAX_ITER = 100000
+
 export function original(element) {
   const act = ()=>{
     for (let i = 0; i < MAX_ITER; i++) {
       const subject = sub();
-      subject.next(genString(10));
-      unsubs.push(subject.subscribe((i) => {
-      }));
+      subject.subscribe((i) => {
+      });
+      subject.next({text:genString(1)});
     }
   }
   element.addEventListener('click', act)
 }
+
 export function withUnsub(element) {
+  const unsubs = []
   const act = ()=>{
     for (let i = 0; i < MAX_ITER; i++) {
       const subject = sub();
-      subject.next(genString(2));
-      subject.subscribe((i) => {
-        setTimeout(()=>{subject.unsubscribe()}, 0)
-      });
+      unsubs.push(subject.subscribe((i) => {
+      }));
+      subject.next({text:genString(1)});
+      for(const unsub of unsubs){
+        unsub.unsubscribe()
+      }
     }
   }
   element.addEventListener('click', act)
@@ -28,10 +33,9 @@ export function takeD(element) {
   const act = ()=>{
     for (let i = 0; i < MAX_ITER; i++) {
       const subject = sub().pipe(take(1));
-      subject.next(genString(2));
       subject.subscribe((i) => {
-        console.log(i[0])
       });
+      subject.next({text:genString(1)});
     }
   }
   element.addEventListener('click', act)
@@ -40,10 +44,10 @@ export function takeWithUnsub(element) {
   const act = ()=>{
     for (let i = 0; i < MAX_ITER; i++) {
       const subject = sub().pipe(take(1));;
-      subject.next(genString(2));
       subject.subscribe((i) => {
         setTimeout(()=>{subject.unsubscribe()}, 0)
       });
+      subject.next({text:genString(1)});
     }
   }
   element.addEventListener('click', act)
